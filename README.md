6ddf4a47b5af997289ce2b3dfe5a9774145164e8
Sackbuoy <cameronkientz@proton.me>
2025-10-17 19:04:34 -0500

    adding lib for use in other flakes

--- README.md
+++ README.md
@@ -17,3 +17,32 @@ kubernetes: 1.33.3
 then just run `nix-vm`
 the first run will download nixpkgs(which will take a while) and search through
 the revision history for the version of the packages specified
+this will also generate a flake in the pwd called `versions` that contains all
+the pinned versions of your packages
+
+## Using in another flake:
+run `nix-vm` to generate `versions/` flake directory
+pull into your flake like so:
+```
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    versions.url = "path:./versions";
+  };
+
+  outputs = { self, nixpkgs, versions }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pinnedPkgs = versions.packages system;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pinnedPkgs; [
+          goreleaser
+          kubernetes
+        ];
+      };
+    };
+}
+```
--- flake.nix
+++ flake.nix
@@ -18,16 +18,9 @@
         packages.default = pkgs.buildGoModule {
           pname = "nix-vm";
           version = "0.1.0";
-
           src = ./.;
-
           vendorHash = "sha256-mcazBBUvRN7bZ/HheOdlNl3F3nfVoiFyVFRtPZ1MxGE=";
-
-          ldflags = [
-            "-s"
-            "-w"
-          ];
-
+          ldflags = ["-s" "-w"];
           meta = with pkgs.lib; {
             description = "Version-pinned package manager for Nix";
             homepage = "https://github.com/yourusername/nix-vm";
@@ -35,6 +28,32 @@
             maintainers = [];
             mainProgram = "nix-vm";
           };
+        };
+
+        # Library functions for use in other flakes
+        lib = {
+          # Function to get a package at a specific version from a commit
+          getPinnedPackage = {
+            name,
+            commit,
+          }: let
+            pinnedPkgs = import (builtins.fetchGit {
+              url = "https://github.com/NixOS/nixpkgs";
+              rev = commit;
+            }) {inherit system;};
+          in
+            pinnedPkgs.${name};
+
+          # Helper to create multiple pinned packages
+          getPinnedPackages = specs:
+            builtins.listToAttrs (map (spec: {
+                name = spec.name;
+                value = self.lib.${system}.getPinnedPackage {
+                  name = spec.name;
+                  commit = spec.commit;
+                };
+              })
+              specs);
         };
 
         # Development shell
